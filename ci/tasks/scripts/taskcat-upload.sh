fd64506f9c0f57db7b020bce4f83e6b1714cc263
Hannes Hörl <hhorl@pivotal.io>
2023-01-12 13:45:22 +0100

    Run `taskcat upload`
    which internally runs `taskcat package` anyway

--- ci/tasks/scripts/taskcat-upload.sh
+++ ci/tasks/scripts/taskcat-upload.sh
@@ -12,23 +12,24 @@ source creds/env.inc.sh
 source ci-repo/ci/tasks/lib/docker.lib.sh
 source ci-repo/ci/tasks/lib/misc.lib.sh
 
-imageCache="${PWD}/image-cache/images.tar.gz"
+readonly IMAGE_CACHE_FILE="${PWD}/image-cache/images.tar.gz"
 
 main() {
-  taskcat package -s functions/source -z functions/packages
+  cd repo
+
+  # Runs `taskcat package -s functions/source -z functions/packages` internally
+  taskcat upload
 }
 
-cd repo
-
 run::logged 'load images from cache' \
-  docker::imageCacheLoad "$imageCache" || true
+  docker::imageCacheLoad "$IMAGE_CACHE_FILE" || true
 run::logged 'preload images from Dockerfiles through proxy' \
-  docker::loadDockerfileFroms . || true
+  docker::loadDockerfileFroms repo || true
 
 rc=0
 run::logged 'main task' main || rc=$?
 
 run::logged 'save images to cache' \
-  docker::imageCacheSave "$imageCache" || true
+  docker::imageCacheSave "$IMAGE_CACHE_FILE" || true
 
 exit $rc
