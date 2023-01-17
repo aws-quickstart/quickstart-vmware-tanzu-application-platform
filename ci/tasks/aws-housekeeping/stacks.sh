cf31fd8e78f3621777aa980abcdf4a59ddedb6ea
Hannes Hörl <hhorl@pivotal.io>
2023-01-17 12:17:30 +0100

    Make stack housekeeper ignore certain stacks
    Also: refactor, to only pull the list of stacks once.

--- ci/tasks/aws-housekeeping/stacks.sh
+++ ci/tasks/aws-housekeeping/stacks.sh
@@ -6,21 +6,30 @@ set -o pipefail
 
 . creds/env.inc.sh
 
+readonly STACK_LIST="${SCRATCH_DIR:-/scratch}/stacks.json"
+
+# ignore nothing, by using an RE that does never match
+readonly IGNORE_OLD_STACKS_RE="${IGNORE_OLD_STACKS_RE:-$^}"
+readonly IGNORE_FAILED_STACKS_RE="${IGNORE_FAILED_STACKS_RE:-$^}"
+
+
 # TODO: auto-cleanup failed stacks; for now, just report
 main() {
   local rc=0
 
+  getStackList "$STACK_LIST"
+
   echo >&2 -e '⚠️ failed stacks:'
-  failedStacks || rc=$(( rc | $? ))
+  failedStacks "$STACK_LIST" || rc=$(( rc | $? ))
 
   echo >&2
   echo >&2 -e '🕧 old stacks:'
-  oldStacks || rc=$(( rc | $? ))
+  oldStacks "$STACK_LIST" || rc=$(( rc | $? ))
 
   return $rc
 }
 
-cleanStackList() {
+printCleanedStacks() {
   jq 'map(del(.TemplateDescription, .DriftInformation))'
 }
 
@@ -29,28 +38,45 @@ isEmpty() {
 }
 
 oldStacks() {
+  local stacks
   local cutOffDate="$( date -u --date "${OLD_AFTER:-3 days} ago" '+%F' )"
-  listStacks \
-    'StackSummaries[?CreationTime<=`'"$cutOffDate"'`]' \
-    'CREATE_COMPLETE'
+
+  stacks="$(
+    jq \
+      --arg ignoreRe "$IGNORE_OLD_STACKS_RE" \
+      --arg cutOffDate "$cutOffDate" \
+      '[ .[] | select(.StackName|test($ignoreRe)|not) | select(.CreationTime < $cutOffDate) ]' \
+      "$1"
+  )"
+
+  printCleanedStacks <<< "$stacks"
+
+  isEmpty <<< "$stacks" || return 1
 }
 
 failedStacks() {
-  listStacks \
-    'StackSummaries[]' \
-    'CREATE_FAILED' 'DELETE_FAILED' 'ROLLBACK_FAILED' 'UPDATE_FAILED'
+  local stacks
+
+  stacks="$(
+    jq \
+      --arg ignoreRe "$IGNORE_FAILED_STACKS_RE" \
+      '[ .[] | select(.StackName|test($ignoreRe)|not) | select(.StackStatus|contains("FAILED")) ]'  \
+      "$1"
+  )"
+
+  printCleanedStacks <<< "$stacks"
+
+  isEmpty <<< "$stacks" || return 1
 }
 
-listStacks() {
-  local query="$1" ; shift
-
+getStackList() {
+  local query='StackSummaries[?StackStatus!=`DELETE_COMPLETE`]'
   local res='[]'
 
   for r in ${REGIONS:-us-east-1} ; do
     regionData="$(
       aws cloudformation list-stacks \
         --region "$r" \
-        --stack-status-filter "$@" \
         --query "$query" \
         --output json \
         | jq --arg region "$r" '.[].Region = $region'
@@ -61,11 +87,7 @@ listStacks() {
     res="$( jq --argjson new "$regionData" '. + $new' <<< "$res" )"
   done
 
-  cleanStackList <<< "$res"
-
-  isEmpty <<< "$res" && {
-    return 1
-  }
+  cat > "$1" <<< "$res"
 }
 
 main "$@"
