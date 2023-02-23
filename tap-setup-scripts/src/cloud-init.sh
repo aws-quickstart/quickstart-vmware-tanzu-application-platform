be33b0d08c1671dfbd366feaa61b8f93a1143c54
leclaist <sleclaire06@gmail.com>
2023-02-22 18:57:40 -0600

    fixing the url to include date

--- tap-setup-scripts/src/cloud-init.sh
+++ tap-setup-scripts/src/cloud-init.sh
@@ -64,8 +64,10 @@ echo "Installing kubectl..."
 pushd /tmp
 # aws s3 cp --no-progress "s3://amazon-eks/${AwsKubectlVersion}/bin/linux/$arch/kubectl" ./kubectl
 # aws s3 cp --no-progress "s3://amazon-eks/${AwsKubectlVersion}/bin/linux/$arch/kubectl.sha256" ./kubectl.sha256
-curl --retry 5 -o ./kubectl "https://amazon-eks.s3.${AWS_REGION}.amazonaws.com/amazon-eks/${AwsKubectlVersion}/bin/linux/$arch/kubectl"
-curl --retry 5 -o ./kubectl.sha256 "https://amazon-eks.s3.${AWS_REGION}.amazonaws.com/amazon-eks/${AwsKubectlVersion}/bin/linux/$arch/kubectl.sha256"
+#The following commands work from 1.21 to latest as of Feb 22 2023
+#Check this url when updating k8s versions - see https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+curl --retry 5 -o ./kubectl "https://s3.us-west-2.amazonaws.com/amazon-eks/${AwsKubectlVersion}/2023-01-30/bin/linux/$arch/kubectl"
+curl --retry 5 -o ./kubectl.sha256 "https://s3.us-west-2.amazonaws.com/amazon-eks/${AwsKubectlVersion}/2023-01-30/bin/linux/$arch/kubectl.sha256"
 openssl sha1 -sha256 ./kubectl
 chmod 755 ./kubectl
 mv ./kubectl /usr/local/bin/
